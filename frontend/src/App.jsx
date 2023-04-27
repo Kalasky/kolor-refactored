7a203c6189e742c2c8c8f0b9db86faf026448241
wiu <kalaskyr@gmail.com>
2023-04-27 09:29:34 -0400

    auth handling for frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,8 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 // pages
 import Home from './pages/Home'
 import Auth from './pages/Auth'
+import AuthSuccess from './pages/AuthSuccess'
+import AuthError from './pages/AuthError'
 
 function App() {
   return (
@@ -12,6 +14,8 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/auth" element={<Auth />} />
+          <Route path="/auth/success" element={<AuthSuccess />} />
+          <Route path="/auth-error" element={<AuthError />} />
         </Routes>
       </BrowserRouter>
     </div>
